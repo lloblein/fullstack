c9e06ec034f5ad90e0b21cb5e91f95c6f0fedfab
lloblein <loblein@hotmail.com>
2023-09-06 17:02:16 -0300

    new feature

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const port = process.env.port || 3001
 const app = express()
 
 
+app.get('/', (req, res) => {
+    console.log(`New feature`)
+})
+
 app.listen(port, () => {
     console.log(`start listening ${port}`)
 })
